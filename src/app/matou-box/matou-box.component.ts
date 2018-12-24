7d39794ca81ca887d3f10bb2403957339561d6ba
AymericPost <aymeric.post@gmail.com>
2018-12-24 18:46:58 +0100

    Suppression des console.log() de débug, Scrolling auto au bas de la chatbox à chaque chargement et mise à jour avec modif.

--- src/app/matou-box/matou-box.component.ts
+++ src/app/matou-box/matou-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewChecked, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { MatouRoarService } from "../matou-roar.service"
 import { MatouFetchService } from "../matou-fetch.service"
 
@@ -10,10 +10,14 @@ import {Observable, interval} from 'rxjs';
   templateUrl: './matou-box.component.html',
   styleUrls: ['./matou-box.component.css']
 })
-export class MatouBoxComponent implements OnInit {
+export class MatouBoxComponent implements OnInit, AfterViewChecked {
+
+   @ViewChild('matou-frame') private myScrollContainer: ElementRef;
 
    private roar:MatouRoarService;
    private fetch:MatouFetchService;
+
+   private needScrolling:boolean = true;
 
    private pseudo:string = "";
    private message:string = "";
@@ -34,22 +38,30 @@ export class MatouBoxComponent implements OnInit {
      secondsCounter.subscribe(() =>
         this.fetch.cacophonie().subscribe(
         (incoming:any[]) => {
-             console.log("Got back : ", incoming)
-             if (this.cacophonie.length != incoming.length) this.cacophonie = incoming;
+             if (this.cacophonie.length != incoming.length) {
+                this.cacophonie = incoming;
+                this.needScrolling = true;
+             }
          })
      );
 
+
   }
 
+  ngAfterViewChecked() {
+    var element = document.getElementById("frame-content");
 
-
+    if(this.needScrolling) {
+      element.scrollTop = element.scrollHeight;
+      this.needScrolling = false;
+      }
+   }
   public miaou(pseudo, message):void {
      this.roar.roar(pseudo, message).subscribe(
-      (incoming:any) => {
-          console.log("Got back : ", incoming)
-      });
+      (incoming:any) => { });
       this.message = "";
 
  }
+
 
 }
--- src/app/matou-roar.service.ts
+++ src/app/matou-roar.service.ts
@@ -15,7 +15,7 @@ export class MatouRoarService {
    }
 
    public roar(pseudo:string, message:string):Observable<any>{
-      console.log("Roar parameters recieved : ", pseudo, message)
+
       const url = "http://192.168.1.13:8042/send?pseudo=" + pseudo + "&text=" + message;
       console.log(url)
     const promise:Observable<any> = this.client.get(url)
